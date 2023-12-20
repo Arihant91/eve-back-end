71d5af92e35094e6410bf54fb0d4ce95ee3c766b
Arihant91 <nsanyi91@gmail.com>
2023-12-20 12:28:47 +0100

    first commit

---
+++ src/test/java/com/backend/eve/EveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.eve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
